007ffc554600c71bfb1afba0c79c0f6c5e45b53c
LuxWise <jcsanchez.martinez.2020@gmail.com>
2023-08-02 05:09:47 -0500

    1.0.0

---
+++ src/Components/ProjectNav/index.jsx
@@ -0,0 +1,13 @@
+const ProjectNav = () => {
+   return (
+      <section className="flex justify-around  bg-sky-500">
+         <input className="w-2/3 px-3 py-2 ring-1 ring-blue-200 rounded-lg outline-none hover:shadow-md hover:shadow-blue-300 duration-400 lg:w-1/3"
+      placeholder="What needs to be done?" />
+         <div>
+            dafascv
+         </div>
+      </section>
+   )
+}
+
+export { ProjectNav }
